a5c920f87d31397df31669bbe0f8987c058b39c9
Grady Kuhnline <github@heygrady.net>
2011-12-26 16:17:40 -0800

    re-organizing the project structure to be much less confusing

---
+++ jquery-corners.js
@@ -0,0 +1,34 @@
+///////////////////////////////////////////////////////
+// Corners
+///////////////////////////////////////////////////////
+;(function($, undefined) {
+    // set up strings
+    var corners = ["tl", "tr", "br", "bl"],
+        sides = ["top", "right", "bottom", "left"],
+        className = "ui-corners",
+        cornerClassName = className + "-corner",
+        sideClassName = className + "-side",
+        spans = "",
+        spanStart  = '<span class="';
+    
+    // creat the span string
+    $.each(corners, function() {
+        spans += spanStart + cornerClassName + ' ' + cornerClassName + '-' + this + '"/>';
+    });
+    $.each(sides, function() {
+        spans += spanStart + sideClassName + ' ' + sideClassName + '-' + this + '"/>';
+    });
+
+    // add corners/sides to an element
+    // wrap the contents with a content div
+    function addCorners(el) {
+        $(el).wrapInner('<div class="' + className + '-content" />').append(spans).addClass(className);
+    }
+
+    // create a jQuery plugin for adding in the corners
+    $.fn.corners = function() {
+        this.not('.' + className).each(function() {
+            addCorners(this);
+        });
+    };
+})(jQuery);
